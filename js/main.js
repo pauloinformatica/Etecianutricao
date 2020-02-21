cf103add36cea1c231445efef92243d5af4fc441
Paulo M Ferreira <paulo.net.informatica@gmail.com>
2020-02-20 22:20:23 -0300

    Criação dinâmica de tabela e inclusão de pacientes

--- js/main.js
+++ js/main.js
@@ -1,41 +1,77 @@
-//console.error("Falha ao inicializar");
-let titulo = document.querySelector("#titulo");
-titulo.textContent = "Etecia Nutrição e Dietética";
+let pacientes = document.querySelectorAll(".paciente");
 
-//pegar o peso
-let paciente = document.querySelector(".paciente");
-let tdpeso = paciente.querySelector(".info-peso");
-let peso = tdpeso.textContent;
-console.log(peso);
+pacientes.forEach(paciente => {
+    let peso = paciente.querySelector(".info-peso").textContent;
+    let altura = paciente.querySelector(".info-altura").textContent;
 
-//pegar altura
-let tdaltura = paciente.querySelector(".info-altura");
-let altura = tdaltura.textContent;
+    let pesoValido = true;
+    let alturaValida = true;
 
-let pesoValido = true;
-let alturaValida = true;
+    let tdIMC = paciente.querySelector(".info-imc");
+
+    if (peso <= 0 || peso > 1000) {
+        pesoValido = false;
+        tdIMC.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura <= 0) {
+        alturaValida = false;
+        tdIMC.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoValido && alturaValida) {
+        let imc = peso / altura ** 2;
+        tdIMC.textContent = imc.toFixed(2);
+    }
+});
+
+let botao = document.querySelector("#botao_cadastrar");
+
+botao.addEventListener("click", function (evento) {
+    evento.preventDefault();
+    let formulario = document.querySelector("#form-paciente");
+    let nome    = formulario.nome.value;
+    let peso    = formulario.peso.value;
+    let altura  = formulario.altura.value;
+    let gordura = formulario.gordura.value;
+    
+    // criar linha
+    
+    let table = document.querySelector("#tabela-pacientes");
+    let tr = document.createElement("tr");
+    tr.classList.add("paciente");
+
+    
+    // criar TD
 
 
-//Validar dados
-if (peso <= 0 || peso > 1000){
+    let tdNome = document.createElement("td");
+    tdNome.textContent = nome;
+    tr.appendChild(tdNome);
 
-    console.error("Peso inválida")
-    pesoValido = false
-}
+    let tdPeso = document.createElement("td");
+    tdPeso.textContent = peso;
+    tr.appendChild(tdPeso);
 
-if (altura <= 0){
+    let tdAltura = document.createElement("td");
+    tdAltura.textContent = altura;
+    tr.appendChild(tdAltura);
 
-    console.error("Altura inválida");
-    alturaValida = false
-}
+    let tdGordura = document.createElement("td");
+    tdGordura.textContent = gordura;
+    tr.appendChild(tdGordura);
 
-if (pesoValido && alturaValida){
 
-//Calcular IMC
-let imc = peso / altura **2;
-console.log(imc);
-}
 
-//escrever o imc na tabela
-let tdimc = paciente.querySelector(".info-imc");
-tdimc.textContent = imc;
+    table.appendChild(tr);
+
+
+
+   // console.log(tr);
+});
+
+    
+
+
+
